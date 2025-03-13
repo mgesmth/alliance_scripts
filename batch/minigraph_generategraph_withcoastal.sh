da8b9166623b84fb9baa4116887a7bdefe0b5c8f
Meg Smith <meg8130@student.ubc.ca>
2025-03-13 16:23:55 -0400

    update

--- batch/minigraph_generategraph_withcoastal.sh
+++ batch/minigraph_generategraph_withcoastal.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=generategraph
 #SBATCH --account=def-booker
-#SBATCH --partition=cpularge_bynode_b4
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=750G
-#SBATCH --time=0-05:00:00
+#SBATCH --time=0-06:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o generategraph_withcoastal.%j.out
 #SBATCH -e generategraph_withcoastal.%j.err
+
+set -o errexit
 
 def_booker=/home/mg615512/projects/def-booker
 interior=${def_booker}/mg615512/minigraph_out/interior_primalt.gfa
@@ -16,7 +17,9 @@ coastal=${def_booker}/shared_dougfir/coastal_douglas_fir/coastal_douglas_fir_202
 out=${def_booker}/projects/def-booker/mg615512/minigraph_out/interior_primalt_coastal
 script=${def_booker}/mg615512/scripts/executables
 
-#minigraph executable
+#executable
 export PATH="${def_booker}/mg615512/bin/minigraph:$PATH"
+export PATH="${def_booker}/mg615512/bin/gfatools:$PATH"
 
-${script}/generate_graph.sh -t 24 -r ${interior} -q ${int_alt} -o ${out}
+${script}/generate_graph.sh -t 24 -r ${interior} -q ${coastal} -o ${out} && \
+${script}/call_SVs_fromgraph.sh -g ${out}.gfa -o ${out}
