0717119ac42b813f33ad9939370ca3af2a650c0f
Meg Smith <meg8130@student.ubc.ca>
2025-03-15 22:25:09 -0400

    rewrote to call alt from primary and coastal from interior (both).

--- batch/call_genome_path.sh
+++ batch/call_genome_path.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-booker
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=250G
-#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=750G
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o asmcall.%j.out
@@ -10,10 +10,14 @@
 
 export PATH="/home/mg615512/projects/def-booker/mg615512/bin/minigraph:$PATH"
 
+booker=/home/mg615512/projects/def-booker
 minidir=/home/mg615512/scratch/minigraph_out
-graph=${minidir}/interior_primalt_renamed_coastal.gfa
-alt=/home/mg615512/projects/def-booker/shared_dougfir/interior_douglas_fir/renamed_scaffolds/interiorDF_alternate_scaffrenamed.fa
-script=/home/mg615512/projects/def-booker/mg615512/scripts/executables
+primary=${booker}/shared_dougfir/interior_douglas_fir/renamed_scaffolds/interiorDF_primary_scaffrenamed.fa
+alt=${booker}/shared_dougfir/interior_douglas_fir/renamed_scaffolds/interiorDF_alternate_scaffrenamed.fa
+interior=${minidir}/interior_primalt_renamed.gfa
+coastal=${booker}/shared_dougfir/coastal_douglas_fir/coastal_douglas_fir_2025_assembly.fasta
+script=${booker}/mg615512/scripts/executables
 
-${script}/call_SVs_fromgraph_minigraph.sh -t 16 -g ${graph} -q ${alt} -o ${minidir}/interior_alt_SVcall
+${script}/call_SVs_fromgraph_minigraph.sh -t 24 -g ${primary} -q ${alt} -o ${minidir}/interior_alt_SVcall
+${script}/call_SVs_fromgraph_minigraph.sh -t 24 -g ${interior} -q ${coastal} -o ${minidir}/coastal_SVcall
 
